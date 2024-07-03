a421ad90911b335ecd63c471356f0400ee472493
dmitriyTASHKINOV <tashkinov.mitiaaaa@gmail.com>
2024-07-03 11:38:41 +0300

    qwe

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,17 +2,19 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Timer timer = context.getBean(Timer.class);
+
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+                    context.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
     }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,22 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    private Timer timer;
+    @Autowired
+    @Qualifier("cat")
     private Animal animal;
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal1;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -16,4 +25,8 @@ public class AnimalsCage {
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
+    public Timer getTimer() {
+        return timer;
+    }
+
 }
